47e4a8d21e9dca07fa5270a04c762be242b63755
iodx2004 <modistotube2004@gmail.com>
2025-11-12 11:26:01 +0000

    1337Pool

---
+++ Code Sources/C02/ex07/ft_strupcase.c
@@ -0,0 +1,23 @@
+char *ft_strupcase(char *str)
+{
+  while(*str)
+  {
+    if(*str >= 'a' && *str <= 'z')
+    {
+      *str -= 32;
+    }
+    str++;
+  }
+  return str;
+}
+
+#include <stdio.h>
+
+int main()
+{
+  char str[] = "Bekkali";
+
+  ft_strupcase(str);
+
+  printf("%s", str);
+}
