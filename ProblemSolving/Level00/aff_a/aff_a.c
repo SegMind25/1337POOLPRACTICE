339f6fa8e0cf3f6d6c66974de52cb02292c8c812
SegMind25 <modistotube2004@gmail.com>
2026-04-07 12:02:37 +0100

    Solve The 1st Problem & Fix : aff_a.c

--- ProblemSolving/Level00/aff_a/aff_a.c
+++ ProblemSolving/Level00/aff_a/aff_a.c
@@ -6,11 +6,16 @@ int main(int ac, char **av)
   if(ac > 1)
   {
     int i = 0;
-    if(av[1][i] == 'a' || av[1][i])
+    if(av[1][i] == 'a')
     {
       write(1, "a", 1);
+      
     }
     write(1, "\n", 1);
+  }
+  else
+  {
+    write(1, "a\n", 2);
   }
 
   return 0;
