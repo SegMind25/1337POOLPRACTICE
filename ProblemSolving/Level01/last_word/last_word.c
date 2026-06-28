56c96784d43ca16028865eacf71c8c638f301c22
segmind25 <modistotube2004@gmail.com>
2026-06-28 14:13:40 +0100

    Solve : last_word.c

---
+++ ProblemSolving/Level01/last_word/last_word.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+
+  int end;
+  if(ac == 2)
+  {
+    int i = 0;
+    while(av[1][i])
+    {
+      i++;
+    }
+    while(i > 0 && (av[1][i - 1] == ' ' || av[1][i - 1] == '\t'))
+    {
+      i--;
+    }
+    end = i;
+    while(i > 0 && (av[1][i - 1] != ' ' && av[1][i - 1] != '\t'))
+    {
+      i--;
+    }
+    while(i < end)
+    {
+      write(1, &av[1][i++], 1);
+    }
+    write(1, "\n", 1);
+  }
+
+  return 0;
+}
