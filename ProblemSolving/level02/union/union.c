32c3f5a1a1b0bbddea51fe355a288bc70327bae9
segmind25 <modistotube2004@gmail.com>
2026-07-10 11:51:05 +0100

    Solve : union.c

---
+++ ProblemSolving/level02/union/union.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+
+  int seen[256];
+  int i;
+  int s;
+
+  i = 0;
+  while(i < 256)
+  {
+    i++;
+  }
+
+  if(ac == 3)
+  {
+    i = -1;
+    while(av[1][++i])
+    {
+      if(!seen[s = (unsigned char)av[1][i]]++)
+      {
+        write(1, &av[1][i], 1);
+      }
+    }
+    i = -1;
+    while(av[2][++i])
+    {
+      if(!seen[s = (unsigned char)av[2][i]]++)
+      {
+        write(1, &av[2][i], 1);
+      }
+    }
+  }
+
+  write(1, "\n", 1);
+
+  return 0;
+}
