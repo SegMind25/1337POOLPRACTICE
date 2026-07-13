961c1be2d27e13b29735416b36df23ff5db7b00a
segmind25 <modistotube2004@gmail.com>
2026-07-13 10:50:17 +0100

    Solve In My Mind : The aff_a.x

--- ProblemSolving/test/Aff_A_Test.c
+++ ProblemSolving/test/Aff_A_Test.c
@@ -4,23 +4,25 @@ int main(int ac, char **av)
 {
 
   int i;
+
   if(ac == 2)
   {
-    i = 0;
     while(av[1][i])
     {
-      if(av[1][i] == 'a')
+      if(av[1][i] != 'a')
       {
-        write(1, "a", 1);
+        i++;
       }
-      i++;
+      else
+      {
+        write(1, "a\n", 2);
+        i++;
+      }
     }
-    write(1, "\n", 1);
   }
   else
   {
-    write(1, "a\n", 2);
+    write(1, "a\n", 1);
   }
-
   return 0;
 }
