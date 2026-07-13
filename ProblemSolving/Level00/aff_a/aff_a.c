b4ac5933e13afbd7b9d698d7e587b53a0725544c
segmind25 <modistotube2004@gmail.com>
2026-07-13 10:49:43 +0100

    Recode : The aff_a.c

--- ProblemSolving/Level00/aff_a/aff_a.c
+++ ProblemSolving/Level00/aff_a/aff_a.c
@@ -3,20 +3,27 @@
 int main(int ac, char **av)
 {
 
-  if(ac > 1)
+  if(ac == 2)
   {
     int i = 0;
-    if(av[1][i] == 'a')
+    while(av[1][i])
     {
-      write(1, "a", 1);
-      
+      if(av[1][i] != 'a')
+      {
+        i++;
+      }
+      else
+      {
+        write(1, "a\n", 2);
+        i++;
+      }
     }
-    write(1, "\n", 1);
   }
   else
   {
     write(1, "a\n", 2);
   }
+
 
   return 0;
 }
