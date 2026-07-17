1079f34fff9db0266cde97cbf242d8df006cc0d3
segmind25 <modistotube2004@gmail.com>
2026-07-17 15:20:38 +0100

    Solve : The ft_strcpy_test.c Problem in My Mind

---
+++ ProblemSolving/test/lvl1/ft_strcpy_test.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+  while(*str)
+  {
+    write(1, str++, 1);
+  }
+}
+
+char *ft_strcpy(char *s1, char *s2)
+{
+  int i = 0;
+  while(s2[i])
+  {
+    s1[i++] = s2[i++];
+  }
+  s1[i] == '\0';
+  return s1;
+}
+
+int main()
+{
+  char str[] = "hello";
+  char dest[22];
+
+  ft_strcpy(dest, str);
+
+  ft_putstr("The Dest : ");
+  ft_putstr(dest);
+  ft_putstr("The Src : ");
+  ft_putstr(str);
+
+  return 0;
+}
