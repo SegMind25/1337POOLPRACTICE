0322c1090ab69936ac014b2baf06524d723b31a7
segmind25 <modistotube2004@gmail.com>
2026-07-17 10:32:02 +0100

    Fix : The problem First_word.c

--- ProblemSolving/Level01/first_word/first_word.c
+++ ProblemSolving/Level01/first_word/first_word.c
@@ -3,17 +3,19 @@
 int main(int ac, char **av)
 {
 
+  int i;
   if(ac == 2)
   {
-    int i = 0;
-    while(av[1][i] == ' ' && av[1][i] == '\t')
+    i = 0;
+    while(av[1][i] == ' ' || av[1][i] == '\t')
     {
       i++;
     }
-    while(av[1][i] && av[1][i] != ' ' || av[1][i] != '\t')
+    while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
     {
       write(1, &av[1][i++], 1);
     }
+    write(1, "\n", 1);
   }
 
   return 0;
